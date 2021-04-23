4ad0390e16213d64272fe7bf577dc23c037dce92
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-23 15:01:56 -0700

    HELP

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,90 @@
     // Refreshes page every 1 second to display time in Real Time
     setInterval(update, 1000);
       
+
+// debugger;
+
+
     // Saves input text when clicking on save button
-      $("button").on("click", function(){
+  $("button").on("click", function(){
+    // debugger;
           $('input, select, textarea').each(function() {
-           var value = $(this).val(),
-               name = $(this).attr('name');
+           var value = $(this).val(), name = $(this).attr('name');
            localStorage[name] = value;
-           console.log(value);   
+           console.log(name, value);   
       })});
 
-$( document ).ready(function() {
-    console.log(localStorage);
-    value = localStorage.getItem("value")
+
+
+
+
+
+
+
+    // $("button").on("click", function(){
+    //     localStorage.setItem("value", JSON.stringify(value));
+    // });
+
+    // var saveTasks = function() {
+    //     localStorage.setItem("value", JSON.stringify(value));
+    //   };
+
+
+// $( document ).ready(function() {
+//     console.log(localStorage);
+//     value = JSON.parse(localStorage.getItem("value"));
+//      // loop over object properties
+//   $.each(value, function(list, arr) {
+//     // console.log(list, arr);
+//     // then loop over sub-array
+//     arr.forEach(function(task) {
+//       createTask(task.text, task.date, list);
+//     });
+//   });
+
+// });
+
+// value = JSON.parse(localStorage.getItem("value"));
+
+
+
+//     $("button").on("click", function(){
+//         $("input, select, textarea", function(){
+//             var value = $(this).val();
+//             localStorage.setItem('this', value)
+//         console.log("clicked!"); 
+//     })
+// });
+    
+
+
+    // $("button").on("click", function(){
+    //           $('input, select, textarea', function() {
+    //            var value = $(this).val(),
+    //                name = $(this).attr('name');
+    //            localStorage[name] = value;
+    //            console.log(value);   
+    //       })});
+
+
+
+    //   $("button").on("click", function(){
+    //       $('input, select, textarea').each(function() {
+    //        var value = $(this).val(),
+    //            name = $(this).attr('name');
+    //        localStorage[name] = value;
+    //        console.log(value);   
+    //   })});
+
+// $( document ).ready(function() {
+//     console.log(localStorage);
+//     value = localStorage.getItem("value")
     //     console.log( "ready!" );
 
     //     $(localStorage).each(function() {
         
         // return(value)
-    });
+    // });
 
 // value = JSON.parse(localStorage.getItem("value"));
 
