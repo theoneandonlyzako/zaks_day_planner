91d89995cdacd877c0d2ec6f65e15e498b9ddb95
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-24 20:54:41 -0700

    almost ready to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+    var something = moment().format('HHHH ');
     // current day on page load
     var update = function() {
-        $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
-        // document.getElementById("currentDay")
-        // .innerHTML = moment().format('dddd, MMMM Do YYYY, h:mm a');
+        // $("#currentDay").text(moment().format("dddd, MMMM Do YYYY,document.getElementById("currentDay").innerHTML;
+       
+        var today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+       
+        document.getElementById("currentDay").innerHTML = today;
     }
     // Refreshes page every .1 second to display time in Real Time
     setInterval(update, 100);
@@ -100,6 +103,113 @@ $("#clear").on("click", function(){
            console.log(name, value);   
            localStorage.removeItem(name, value);
       }).then(location.reload())});
+
+//  Changes text area color based on TOD for each row
+function getStylesheet() {
+    var currentTime = something
+    console.log(something);
+    
+    // 9:00am
+    if (currentTime < 0900) {
+    var element = document.getElementById("nine");
+    element.classList.add("future");
+} else if (currentTime <= 0959) {
+    var element = document.getElementById("nine");
+    element.classList.add("present");
+} else if (currentTime > 0959) {
+    var element = document.getElementById("nine");
+    element.classList.add("past");
+} 
+// 10:00am
+    if (currentTime < 1000) {
+    var element = document.getElementById("ten");
+    element.classList.add("future");
+} else if (currentTime <= 1059) {
+    var element = document.getElementById("ten");
+    element.classList.add("present");
+} else if (currentTime > 1059) {
+    var element = document.getElementById("ten");
+    element.classList.add("past");
+} 
+// 11:00am
+    if (currentTime < 1100) {
+    var element = document.getElementById("eleven");
+    element.classList.add("future");
+} else if (currentTime <= 1159) {
+    var element = document.getElementById("eleven");
+    element.classList.add("present");
+} else if (currentTime > 1159) {
+    var element = document.getElementById("eleven");
+    element.classList.add("past");
+} 
+//  12:00pm
+    if (currentTime < 1200) {
+    var element = document.getElementById("twelve");
+    element.classList.add("future");
+} else if (currentTime <= 1259) {
+    var element = document.getElementById("twelve");
+    element.classList.add("present");
+} else if (currentTime > 1259) {
+    var element = document.getElementById("twelve");
+    element.classList.add("past");
+} 
+//  1:00pm
+    if (currentTime < 1300) {
+    var element = document.getElementById("one");
+    element.classList.add("future");
+} else if (currentTime <= 1359) {
+    var element = document.getElementById("one");
+    element.classList.add("present");
+} else if (currentTime > 1359) {
+    var element = document.getElementById("one");
+    element.classList.add("past");
+} 
+// 2:00pm
+    if (currentTime < 1400) {
+    var element = document.getElementById("two");
+    element.classList.add("future");
+} else if (currentTime <= 1459) {
+    var element = document.getElementById("two");
+    element.classList.add("present");
+} else if (currentTime > 1459) {
+    var element = document.getElementById("two");
+    element.classList.add("past");
+} 
+    // 3:00pm
+    if (currentTime < 1500) {
+        var element = document.getElementById("three");
+        element.classList.add("future");
+    } else if (currentTime < 1559) {
+        var element = document.getElementById("three");
+        element.classList.add("present");
+    } else if (currentTime > 1559) {
+        var element = document.getElementById("three");
+        element.classList.add("past");
+    } 
+       // 4:00pm
+       if (currentTime > 1700) {
+        var element = document.getElementById("four");
+        element.classList.add("future");
+    } else if (currentTime <= 1659 || currentTime >= 1559) {
+        var element = document.getElementById("four");
+        element.classList.add("present");
+    } else if (currentTime > 1659) {
+        var element = document.getElementById("four");
+        element.classList.add("past");
+    } 
+           // 5:00pm
+        if (currentTime <= 1759) {
+        var element = document.getElementById("five");
+        element.classList.add("future");
+    } else if (currentTime < 1800) {
+        var element = document.getElementById("five");
+        element.classList.add("present");
+    } else if (currentTime > 1759) {
+        var element = document.getElementById("five");
+        element.classList.add("past");
+    } 
+};
+ getStylesheet()
 
 // function getStylesheet() {
 //     var currentTime = new Date().getHours();
